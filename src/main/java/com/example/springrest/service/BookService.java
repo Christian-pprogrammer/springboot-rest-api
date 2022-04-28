d8523ab20caee00ff971fc0800fdac753266ff1e
Christian-pprogrammer <mpanoc6@gmail.com>
2022-04-28 10:30:42 +0200

    add book service

--- src/main/java/com/example/springrest/service/BookService.java
+++ src/main/java/com/example/springrest/service/BookService.java
@@ -1,4 +1,19 @@
 package com.example.springrest.service;
 
+import com.example.springrest.model.BookModel;
+import com.example.springrest.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Book;
+import java.util.List;
+
+@Service
 public class BookService {
+    private final BookRepository bookRepository;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public List<BookModel> getBooks() {
+        return this.bookRepository.findAll();
+    }
 }
